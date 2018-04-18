14f2005f2d1053c7d47de28369d8c61718b58d87
Chris Harvey <chharvey@icloud.com>
2018-04-18 20:09:25 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def show
     @post = Post.find(params[:id])
   end
@@ -9,7 +10,6 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
